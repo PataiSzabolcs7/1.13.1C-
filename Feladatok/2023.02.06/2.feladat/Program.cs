83547d13108f4f8bf5f186b15b802962414a9c8d
PataiSzabolcs7 <112942021+PataiSzabolcs7@users.noreply.github.com>
2023-02-06 12:09:24 +0100

    Add files via upload

---
+++ Feladatok/2023.02.06/2.feladat/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Adj meg egy számot: ");
+            x = int.Parse(Console.ReadLine());
+            if (x % 3 == 0) 
+            {
+                Console.WriteLine("A Szám osztható 3-mal");
+            }
+            if (x % 4 == 0) 
+            {
+                Console.WriteLine("A szám osztható 4-el");
+            }
+            if (x % 9== 0) 
+            {
+                Console.WriteLine("A szám osztható 9-el");
+            }
+            Console.ReadKey();
+        }   
+    }
+}
