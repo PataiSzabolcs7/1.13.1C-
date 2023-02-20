f80f000699b855423a169001eafe9bbd01f3c772
PataiSzabolcs7 <112942021+PataiSzabolcs7@users.noreply.github.com>
2023-02-20 09:37:27 +0100

    Add files via upload

---
+++ Hazifeladat 2023.02.19 ( Nem teljes)/Feladat 1.2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feladat_1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "0111011010111101001010101011111110111";
+            int islandCount = 0;
+            int maxIslandLength = 0;
+            int i = 0;
+
+            while (i < data.Length)
+            {
+                if (data[i] == '1')
+                {
+                    ++islandCount;
+                    int j = i;
+                    int tmp = 0;
+
+                    while (j < data.Length && data[j] == '1')
+                    {
+                        ++j;
+                        ++tmp;
+                    }
+                    i = j;
+
+                    if (tmp > maxIslandLength) { maxIslandLength = tmp; }
+                }else
+                {
+                    ++i;
+                }
+            }
+            Console.WriteLine("A sziget száma:{0}", islandCount);
+            Console.WriteLine("A leghosszab sziget: {0}", maxIslandLength);
+            Console.ReadKey();
+        }
+    }
+}
