828889fe9c2b081ccf836ae29ab268b2eaf653a3
PataiSzabolcs7 <112942021+PataiSzabolcs7@users.noreply.github.com>
2023-02-20 11:32:05 +0100

    Add files via upload

---
+++ Gyakorlas/Osztaly/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osztaly
+{   
+    class kutya
+    {
+        //Properties / osztály változók / osztály tagjai
+        private string nev;
+        private int gender;
+        public string Fajta { get; set; }
+        // Konstruktorok
+
+        public void setGender(string gender)
+        {
+            switch (gender)
+            {
+                case "Kan":
+                    this.gender= 1; 
+                    break;
+                case "Szuka":
+                    this.gender = 2;
+                    break;
+                default: 
+                    this.gender = 3;
+                    break;
+            }
+        }
+        public string getGender()
+        {
+            switch(this.gender)
+            {
+                case 1:
+                    return "Kan";
+                case 2:
+                    return "Szuka";
+                default:
+                    return "ismeretlen";
+            }
+        }
+        public kutya(string nev) 
+        {
+            // a This ( Rámutatt arra az objektumra ahova írjuk)
+            this.nev = nev;
+        }
+
+        public kutya() 
+        {
+
+        }
+
+        public string getNev() => this.nev; // Lambda operátoros felírás
+
+        public void setNev(string nev) 
+        {
+            this.nev = nev;
+        }
+    }
+    internal class Program
+    {   
+        static void Main(string[] args)
+        {
+            kutya kutya1 = new kutya();
+            kutya1.setNev("BODRI TE KUTYA");
+
+            Console.WriteLine("A kutya neve: {0}", kutya1.getNev());
+
+            kutya zeusz = new kutya("Zeusz");
+            Console.WriteLine("A kutya neve: {0}", zeusz.getNev());
+
+            zeusz.Fajta = "Német Juhász";
+            Console.WriteLine("A Zeusz fajtája: {0}", zeusz.Fajta);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Gyakorlas/Polimorf/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorf
+{
+    class Ember
+    {
+        public string Nev { get; set; }
+        private string bemut = "Üdvözlöm a nevem:";
+
+        public Ember(string nev)
+        {
+            Nev = nev;
+        }
+
+        public string beszel()
+        {
+            return (bemut + Nev);
+        }
+    }
+
+    class Ferfi : Ember
+    {
+        public int Kor { get; set; }
+        public Ferfi(string nev, int kor) : base(nev)
+        {
+            Kor = kor;
+        }
+    }
+
+    class Woman : Ember
+    {
+        public string SzemSzin { get; set; }
+        public Woman(string nev,string szemszin) : base(nev) 
+        {
+            SzemSzin= szemszin;
+        }
+    }
+    class haz
+    {
+        private List<Ember> lakok = new List<Ember>();
+
+        public void hazajon(Ember obj)
+        {
+            this.lakok.Add(obj); 
+        }
+        public void csenget()
+        {
+            int i;
+            Random rd = new Random();
+            if(lakok.Count> 0) 
+            {
+                i = rd.Next (0, lakok.Count-1);
+                Console.WriteLine(lakok[i].beszel());
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            haz otthon = new haz();
+
+            Woman eva = new Woman("Éva", "zöld");
+            Woman kati = new Woman("Kati", "barna");
+            Woman sari = new Woman("Sári","kék");
+            Woman zsuzsi = new Woman("Zsuzsi", "zöld");
+
+            Ferfi adam = new Ferfi("Ádám", 26);
+            Ferfi dani = new Ferfi("Dani", 26);
+            Ferfi gergo = new Ferfi("Gergő", 26);
+            Ferfi atti = new Ferfi("Attila", 26);
+
+            if (eva is Ember) otthon.hazajon(eva as Ember);
+                otthon.hazajon((Ember)kati);
+                otthon.hazajon((Ember)sari);
+                otthon.hazajon((Ember)zsuzsi);
+                otthon.hazajon((Ember)adam);
+                otthon.hazajon((Ember)dani);
+                otthon.hazajon((Ember)gergo);
+                otthon.hazajon((Ember)atti);
+
+            bool tovabb = true;
+            do
+            {
+                otthon.csenget();
+                tovabb = (Console.ReadLine() == String.Empty?
+                    false: true);
+            }while (tovabb);
+            Console.ReadKey();  
+        }
+    }
+}
