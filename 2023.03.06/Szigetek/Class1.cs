40cc222e7793e93222f8fa4d20f0bfd144386115
PataiSzabolcs7 <112942021+PataiSzabolcs7@users.noreply.github.com>
2023-03-06 10:55:11 +0100

    Add files via upload

---
+++ 2023.03.06/Szigetek/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szigetek
+{
+    internal class Szigetszamolas
+    {
+        // Osztályváltozó
+        int islandcount = 0,
+            maxIslandLength = 0,
+            i = 0;
+        string data = string.Empty;
+        //Konstuktorok
+        public Szigetszamolas() { }
+
+        public Szigetszamolas(string data)
+        {
+            this.data = data;
+        }
+        //Metódusok
+
+        public void setData(string data)
+        {
+            this.data = data;
+        }
+        public string getData() 
+        { 
+            return this.data;
+        }
+        public int getIslandCount()
+        {
+            return this.islandcount;
+        }
+        public int getMaxIslandLength()
+        {
+            return this.maxIslandLength;
+        }
+        public void Szamol()
+        {
+            while (i < data.Length)
+            {
+                if (this.data[i] == '1')
+                {
+                    ++this.islandcount;
+                    int j = i;
+                    int tmp = 0;
+                    while (j < this.data.Length && data[j] == '1')
+                    {
+                        ++j; ++tmp;
+                    }
+                    i = j;
+                    if (tmp > this.maxIslandLength)
+                    {
+                        this.maxIslandLength = tmp;
+                    }
+                }
+                else
+                {
+                    ++i;
+                }
+            }
+        }
+    }
+}
---
+++ 2023.03.06/Szigetek/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szigetek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1.Példa
+            if (true) 
+            { 
+                string s = "001111111101110000001111110011110";
+                Szigetszamolas sz1 = new Szigetszamolas();
+                sz1.setData(s);
+                sz1.Szamol();
+                Console.WriteLine($"A szigetek száma:{sz1.getIslandCount()}, A leghosszabb sziget hossza:{sz1.getMaxIslandLength()}");
+            }
+            #endregion
+            #region 2.Példa
+            if (true)
+            {
+            string s2 = "11111111111110000000000001111111110000000000111111111110101010101000000000111111111111111111000000000111011111111111111100000001111010000011100011001000";
+            Szigetszamolas sz2 = new Szigetszamolas();
+            sz2.setData(s2);
+            sz2.Szamol();
+            Console.WriteLine($"A szigetek száma:{sz2.getIslandCount()}, A leghosszabb sziget hossza:{sz2.getMaxIslandLength()}");
+            }
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
