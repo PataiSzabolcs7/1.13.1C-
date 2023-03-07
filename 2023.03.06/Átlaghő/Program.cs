a363461b9c4d18bc746bd7c34e4be419cbab0a8b
PataiSzabolcs7 <112942021+PataiSzabolcs7@users.noreply.github.com>
2023-03-07 02:03:57 +0100

    Add files via upload

---
+++ 2023.03.06/Átlaghő/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Átlaghő
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int honapok = 12;
+            const int napok = 30;
+            double[,] homerseklet = new double[honapok, napok];
+
+            Random rand = new Random();
+            for (int i = 0; i < honapok; i++)
+            {
+                for (int j = 0; j < napok; j++)
+                {
+                    homerseklet[i, j] = rand.NextDouble() * 40 - 10;
+                }
+            }
+
+            double maxhő = double.MinValue;
+            double minhő = double.MaxValue;
+
+            for (int i = 0; i < honapok; i++)
+            {
+                for (int j = 0; j < napok; j++)
+                {
+                    if (homerseklet[i, j] > maxhő)
+                    {
+                        maxhő = homerseklet[i, j];
+                    }
+                    if (homerseklet[i, j] < minhő)
+                    {
+                        minhő = homerseklet[i, j];
+                    }
+                }
+            }
+            Console.WriteLine("A legmelegebb fok:{0}", maxhő);
+            Console.WriteLine("A leghidegebb fok:{0}", minhő);
+            Console.ReadKey();
+
+        }
+    }
+}
