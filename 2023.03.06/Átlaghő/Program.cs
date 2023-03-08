fc8fc639918bb06eee58b927596cc8fe902f2e84
PataiSzabolcs7 <112942021+PataiSzabolcs7@users.noreply.github.com>
2023-03-08 17:23:25 +0100

    Update Program.cs

--- 2023.03.06/Átlaghő/Program.cs
+++ 2023.03.06/Átlaghő/Program.cs
@@ -1,49 +1,69 @@
-﻿using System;
+﻿[17:13] Patai Szabolcs Lajos
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
-namespace Átlaghő
+using System.Threading.Tasks; namespace Átlaghő
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            const int honapok = 12;
-            const int napok = 30;
-            double[,] homerseklet = new double[honapok, napok];
-
-            Random rand = new Random();
-            for (int i = 0; i < honapok; i++)
-            {
-                for (int j = 0; j < napok; j++)
-                {
-                    homerseklet[i, j] = rand.NextDouble() * 40 - 10;
-                }
-            }
-
-            double maxhő = double.MinValue;
-            double minhő = double.MaxValue;
-
-            for (int i = 0; i < honapok; i++)
-            {
-                for (int j = 0; j < napok; j++)
-                {
-                    if (homerseklet[i, j] > maxhő)
-                    {
-                        maxhő = homerseklet[i, j];
-                    }
-                    if (homerseklet[i, j] < minhő)
-                    {
-                        minhő = homerseklet[i, j];
-                    }
-                }
-            }
-            Console.WriteLine("A legmelegebb fok:{0}", maxhő);
-            Console.WriteLine("A leghidegebb fok:{0}", minhő);
-            Console.ReadKey();
-
-        }
-    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int honapok = 12;
+            int napok = 30;
+            int[,] homerseklet = new int[honapok, napok];             Random rand = new Random();
+            for (int i = 0; i < honapok; i++)
+            {
+                for (int j = 0; j < napok; j++)
+                {
+                    homerseklet[i, j] = rand.Next(-20,40);
+                }
+            }             int maxho = int.MinValue;
+            int minho = int.MaxValue;             for (int i = 0; i < honapok; i++)
+            {
+                for (int j = 0; j < napok; j++)
+                {
+                    if (homerseklet[i, j] > maxho)
+                    {
+                        maxho = homerseklet[i, j];
+                    }
+                    if (homerseklet[i, j] < minho)
+                    {
+                        minho = homerseklet[i, j];
+                    }
+                }
+            }
+            Console.WriteLine("A legmelegebb fok:{0}", maxho);
+            Console.WriteLine("A leghidegebb fok:{0}", minho);             int[] honapho = new int[honapok];
+            for (int i = 0; i < honapok; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < napok; j++)
+                {
+                    sum += homerseklet[i, j];
+                }
+                honapho[i] = sum / napok;
+            }
+            int maxhonapho = int.MinValue;
+            int minhonapho = int.MaxValue;
+            int maxhonap = 0;
+            int minhonap = 0;
+            for (int i = 0; i <  honapok; i++)
+            {
+                if (honapho[i] > maxhonapho)
+                {
+                    maxhonapho = honapho[i];
+                    maxhonap = i + 1;
+                }
+                if (honapho[i] < minhonapho)
+                {
+                    minhonapho = honapho[i];
+                    minhonap = i + 1;
+                }
+            }
+            Console.WriteLine("A legmelegebb hónap:{0}-dik, az átlagoshőmérséklet:{1} fok",maxhonap,maxhonapho);
+            Console.WriteLine("A leghidegebb hónap:{0}-dik, az átlagoshőmérséklet:{1} fok",minhonap,minhonapho); 
+            Console.ReadKey();         }
+    }
 }
+
