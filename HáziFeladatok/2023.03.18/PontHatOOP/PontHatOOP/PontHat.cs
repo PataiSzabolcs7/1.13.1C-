ec2dd8b0ac28771a09fc20efc8bd4b7042949207
PataiSzabolcs7 <112942021+PataiSzabolcs7@users.noreply.github.com>
2023-03-18 18:45:47 +0100

    Add files via upload

---
+++ HáziFeladatok/2023.03.18/PontHatOOP/PontHatOOP/PontHat.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PontHatOOP
+{
+    internal class PontHat
+    {
+        private int a;
+
+        public PontHat() { }
+        public PontHat(int a) 
+        {
+            this.a = a;
+        }
+        public void setA(int a)
+        {
+            this.a = a;
+        }
+        public int getA() 
+        {   
+            if(this.a > 100 && this.a<0)
+            {
+                Console.WriteLine("Hibás pontszám!");
+            }
+            else if (this.a < 50)
+            {
+                Console.WriteLine("Egyes, a pont száma:{0}",this.a );
+            }
+            else if (this.a >= 50 && this.a < 65)
+            {
+                Console.WriteLine("Kettes, a pont száma:{0}", this.a);
+            }
+            else if (this.a >= 65 && this.a < 80)
+            {
+                Console.WriteLine("Hármas, a pont száma:{0}", this.a);
+            }
+            else if (this.a >= 80 && this.a < 90)
+            {
+                Console.WriteLine("Négyes, a pont száma:{0}", this.a);
+            }
+            else if (this.a >= 90)
+            {
+                Console.WriteLine("Öttös, a pont száma:{0}", this.a);
+            }
+            return this.a;
+        }
+    }
+}
---
+++ HáziFeladatok/2023.03.18/PontHatOOP/PontHatOOP/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PontHatOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PontHat h1 = new PontHat();
+            h1.setA(90);
+            Console.WriteLine(h1.getA());
+            Console.ReadKey();
+        }
+    }
+}
